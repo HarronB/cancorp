0b2a1b1af914cc8010f1ad087cc0f103b60b1368
HarronB <bharron@academic.rrc.ca>
2019-04-04 17:48:37 -0500

    Added restful routes and set root to products controller index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'customers/index'
-  get 'customers/show'
-  get 'categories/index'
-  get 'categories/show'
-  get 'products/index'
-  get 'products/show'
+  # get 'orders/index'
+  # get 'orders/show'
+  # get 'customers/index'
+  # get 'customers/show'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'products/index'
+  # get 'products/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # create the index and show restful routes
+  resources :product, only: %i[index show]
+  resources :customers, only: %i[index show]
+  resources :orders, only: %i[index show]
+  resources :categories, only: %i[index show]
+
+  root to: 'products#index' # root to products controller index action
 end
