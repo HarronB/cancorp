0ce03905d104f1f9250c4d305be238c74ca20090
HarronB <bharron@academic.rrc.ca>
2019-04-04 17:39:46 -0500

    Created Instance Variables in Controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class CategoriesController < ApplicationController
   def index
+    @products = Product.order(:name)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class OrdersController < ApplicationController
   def index
+    @order = Order.order(:date)
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 end
