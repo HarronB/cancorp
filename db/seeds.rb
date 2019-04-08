78ecb101634c5efbcf4c8ba70005de79a1258976
HarronB <bharron@academic.rrc.ca>
2019-04-07 23:40:10 -0500

    Added products resource to give aactive admin access to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ Category.destroy_all
 3.times do
   cat = Category.create(name: Faker::Cannabis.unique.type)
 
-  5.times do
+  10.times do
     cat.products.create(name: Faker::Cannabis.unique.strain,
                         price: Faker::Commerce.price(range = 6..12.0, as_string = false),
                         description: Faker::Cannabis.unique.health_benefit)
@@ -29,4 +29,5 @@ end
 
 puts "Number of products: #{Product.count}"
 puts "Number of cats: #{Category.count}"
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
