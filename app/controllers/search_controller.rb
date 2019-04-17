5e69fbbee64ad26b41c6d091b0f8c1a9bf65f429
HarronB <bharron@academic.rrc.ca>
2019-04-17 13:00:53 -0500

    Added pictures to shopping car row view and search results view.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
   def results
     @query = params[:query]
     @queryc = params[:queryc]
-    @products = Product.where('name LIKE ?', "%#{@query}%").where('category_id LIKE ?', "%#{@queryc}%")
+    @products = Product.where('name LIKE ?', "%#{@query}%")
+                       .where('category_id LIKE ?', "%#{@queryc}%")
   end
 end
